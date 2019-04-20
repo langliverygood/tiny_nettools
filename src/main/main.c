7e8c157fd0a8f24d90aa05ac8e992e565cce6627
langliverygood <951328469@qq.com>
2019-04-20 22:54:33 +0800

    添加系统命令ifconfig

--- src/main/main.c
+++ src/main/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <pthread.h>
 
 #include "lshell.h"
 #include "sniffer.h"
+#include "arp.h"
+
+void func_arp_deceive(int argc, char **argv)
+{
+	arp_deceive(argv[0], argv[1], argv[2], 0);
+	
+	return;
+}
 
 void func_start_sniffer(int argc, char **argv)
 {
@@ -21,16 +31,21 @@ void func_stop_sniffer(int argc, char **argv)
 	return;
 }
 
-void test_ls(int argc, char **argv)
+void run_ifconfig(int argc, char **argv)
 {
-	int i;
+	char buf[4096];
+	int fd, cnt;
+	FILE *fp; 
 	
-	printf("ls\n");
-	printf("%d\n", argc);
-	for(i = 0; i < argc; i++)
+	fp = popen("ifconfig", "r"); 
+	if(fp)
 	{
-		printf("%s\n", argv[i]);
+		fd = fileno(fp);
+		cnt = read(fd, buf, sizeof(buf));
+		write(1, buf, cnt);
 	}
+	pclose(fp);
+	
 	
 	return;
 }
@@ -42,7 +57,8 @@ int main(int argc, char **argv)
 	lshell_init();
 	lshell_set_promt("tiny_nettools");
 	lshell_set_errmsg_swtich(1);
-	lshell_register(-1, "ls", "ls", test_ls, RUN_AT_NEW_THREAD);
+	lshell_register(-1, "ifconfig", "ifconfig", run_ifconfig, RUN_AT_MAIN_THREAD);
+	lshell_register(-1, "arp", "arp", func_arp_deceive, RUN_AT_MAIN_THREAD);
 	ret = lshell_register(-1, "sniffer", "sniffer", func_start_sniffer, RUN_AT_NEW_THREAD);
 	lshell_register(ret, "stop", "sniffer stop", func_stop_sniffer, RUN_AT_NEW_THREAD);
 	lshell_start();
